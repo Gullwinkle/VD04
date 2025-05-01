e970d429d04e9055dc55f52b316f579e9c64831d
Gullwinkle <gullwinkle@gmail.com>
2025-05-01 17:15:04 +0500

    VD05

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     context = {
         'link': 'Подробнее'
     }
-    return render_template('index.html', **context)
+    return render_template('home.html', **context)
 
 
 @app.route('/blog/')
 def blog():
     return render_template('blog.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 
 @app.route('/contacts/')
